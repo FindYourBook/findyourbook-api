343da4b0e1782f18c72d3e202417043898489102
Luiza Barros <thecoderana@gmail.com>
2023-01-26 15:32:22 -0300

    fix: duplicate import

--- src/routes/posts.routes.ts
+++ src/routes/posts.routes.ts
@@ -1,6 +1,5 @@
 import { Router } from "express"
-import { createPostsController, updatePostsController } from "../controllers/posts.controllers"
-import { createPostsController, getAllPostsController } from "../controllers/posts.controllers"
+import { createPostsController, updatePostsController, getAllPostsController } from "../controllers/posts.controllers"
 import ensureAuthMiddleware from "../middlewares/ensureAuth.middleware"
 import ensureUserIsPostOwnerMiddleware from "../middlewares/ensureUserIsPostOwner.middleware"
 
