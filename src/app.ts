ccdc6a2ee5b61910280e7acb1d0243e58052ed82
Luiza Barros <thecoderana@gmail.com>
2023-03-12 08:27:21 -0300

    feat: add config for docs

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import express from "express"
 import errorHandler from "./errors/errorHandler"
 import usersRoutes from "./routes/users.routes"
 import postsRoutes from "./routes/posts.routes"
+import { Request, Response } from "express"
 
 const app = express()
 app.use(express.json())
@@ -11,5 +12,10 @@ app.use(errorHandler)
 
 app.use("/users", usersRoutes)
 app.use("/posts", postsRoutes)
+
+app.use(express.static("documentation"))
+app.use("./", (req: Request, res: Response) => {
+    res.render("index.html")
+})
 
 export default app
